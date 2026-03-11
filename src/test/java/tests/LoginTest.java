4ac14fcb924a328414900f6a880e8fe45b30a5d8
f.c. <fatihcandas@gmail.com>
2026-03-12 01:49:10 +0300

    Initial Selenium POM framework setup

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void loginTest(){
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
+
+    }
+
+}
